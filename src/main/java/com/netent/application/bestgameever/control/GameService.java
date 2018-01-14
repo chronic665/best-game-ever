aefbde292fdb998f49dd346d7546932e72df35e7
Chronic <chronic6666@gmail.com>
2018-01-14 15:20:14 +0100

    Added simple round calculation

--- src/main/java/com/netent/application/bestgameever/control/GameService.java
+++ src/main/java/com/netent/application/bestgameever/control/GameService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import static java.lang.Math.random;
+
 @Service
 public class GameService {
 
@@ -23,29 +25,40 @@ public class GameService {
     }
 
     public String play(final String username) {
-        if(personRepository.find(username) == null) {
+        if (personRepository.find(username) == null) {
             throw new UserDoesNotExistException(username);
         }
         // 1. get rules
         final GameConfig config = gameRepository.getGameConfig();
         // 2. play round
-            // 2.1 throw dice
-            final ResultType result = throwDice(config);
-            // 2.3 store result
-            final String roundId = gameRepository.storeRound(result, username);
-            // 2.4 update balance
-            double currentBalance = personRepository.updateBalance(username, result, config);
-            if(currentBalance <= config.getCost()) {
-                gameRepository.storeRound(ResultType.FILL_UP_BALANCE, username);
-                personRepository.updateBalance(username, ResultType.FILL_UP_BALANCE, config);
-            }
-        // 3. prepare response
+        // 2.1 throw dice
+        final ResultType result = throwDice(config);
+        // 2.3 store result
+        final String roundId = gameRepository.storeRound(result, username);
+        // 2.4 update balance
+        double currentBalance = personRepository.updateBalance(username, result, config);
+        if (currentBalance <= config.getCost()) {
+            gameRepository.storeRound(ResultType.FILL_UP_BALANCE, username);
+            personRepository.updateBalance(username, ResultType.FILL_UP_BALANCE, config);
+        }
+        // 3. return roundId
         return roundId;
     }
 
     private ResultType throwDice(final GameConfig config) {
-        // TODO move into extra service
-        return ResultType.LOSE;
+        ResultType type = ResultType.LOSE;
+        // roll dice for win
+        if (Math.random() <= 0.3) {
+            type = ResultType.WIN;
+        }
+        // roll extra dice for free round
+        if (Math.random() <= 0.1) {
+            // add the free round to the previously rolled result type
+            type = (type == ResultType.WIN)
+                    ? ResultType.WIN_AND_FREE_ROUND
+                    : ResultType.FREE_ROUND;
+        }
+        return type;
     }
 
     public Flux<RoundResult> subscribeToResults(final String username, final String roundId) {
